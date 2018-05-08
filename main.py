b23d7aea2e7951fed34d0916f04fb67b82893fc8
Peter-Severynen <peterseverynen@gmail.com>
2018-05-08 17:35:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Main.py is the script that will run all other files.
+Authors:
+Peter Severynen 1000932156
+Carlos Bustos 1001317137
 """
 
 """
@@ -8,9 +11,7 @@ To import a file just use import file_name
 Do NOT add the .py
 RENAME THE FILE IF IT HAS A SPACE
 """
-#execute serverlog.py
-import serverlog
-import tcp_packet
+
 import routerA
 import routerB
 import routerC
@@ -19,3 +20,6 @@ import routerE
 import routerF
 import routerG
 import routerL
+#execute serverlog.py
+import serverlog
+import tcp_packet

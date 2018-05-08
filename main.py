ae89c1d2a514456046bccca3fd100da2c303a37b
CarlosBustos608 <33190811+CarlosBustos608@users.noreply.github.com>
2018-05-08 16:21:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Main.py is the script that will run all other files.
+"""
+
+"""
+To import a file just use import file_name 
+Do NOT add the .py
+RENAME THE FILE IF IT HAS A SPACE
+"""
+#execute serverlog.py
+import serverlog
+import tcp_packet
